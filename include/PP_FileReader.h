c3bbe7924a035055600fae7a33e217908b83d763
Denis Cooper <denis.cooper09@gmail.com>
2025-03-04 00:20:49 +0000

    Add file reader.
    This will be used for loading shaders.

---
+++ include/PP_FileReader.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "PP_Debug.h"
+
+void PP_ReadFile(const char *restrict path, char **restrict output);
---
+++ src/PP_FileReader.c
@@ -0,0 +1,27 @@
+#include "PP_FileReader.h"
+
+void PP_ReadFile(const char *restrict path, char **restrict output)
+{
+    FILE *file = fopen(path, "rb");
+
+    if (!file)
+    {
+        PP_ERROR("Unable to open file - Path: %s", path);
+        return;
+    }
+
+    fseek(file, 0L, SEEK_END);
+    const long size = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+
+    *output = malloc((size + 1) * sizeof(char));
+
+    if (!*output)
+    {
+        PP_ERROR("Failed to allocate memory.");
+        return;
+    }
+
+    fread(*output, sizeof(char), size, file);
+    *(*output + size) = '\0';
+}
